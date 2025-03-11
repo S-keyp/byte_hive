4cb9ef3ed7871b2421ddb4d414ffbb6d98182972
s-keyp <vicodin@orange.fr>
2025-03-11 10:29:12 +0100

    feat: footer under 800px

--- components/Footer/index.vue
+++ components/Footer/index.vue
@@ -1,11 +1,12 @@
 <template>
     <footer>
         <Container>
-            <ul>
-                <li><NuxtLink to="/">Accueil</NuxtLink></li>
-                <li><NuxtLink to="/expertise">A propos</NuxtLink></li>
-                <li><NuxtLink to="/contact">Contact</NuxtLink></li>
-                <li><NuxtLink to="/websites">Cas pratiques</NuxtLink></li>
+            <ul class="footer-items">
+                <li class="footer-item"><NuxtLink to="/">Accueil</NuxtLink></li>
+                <li class="footer-item"><NuxtLink to="/expertise">A propos</NuxtLink></li>
+                <li class="footer-item"><NuxtLink to="/contact">Contact</NuxtLink></li>
+                <li class="footer-item"><NuxtLink to="/websites">Cas pratiques</NuxtLink></li>
+                <li class="footer-item"><a href="#">Back to top</a></li>
             </ul>
         </Container>
     </footer>
